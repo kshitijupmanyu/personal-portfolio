1265b76a563ae94295350b149875b82864316d96
Kshitij Upmanyu <kshitijupmanyu101@gmail.com>
2023-02-15 16:22:16 +0530

    made some changes and redeployed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: 'class',
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
